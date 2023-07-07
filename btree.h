f123622f0a2a00cebb9c8439602ba22de8a883c9
ubuntu <ubuntu@localhost.localdomain>
2023-07-07 15:41:18 +0800

    add node and btree class

---
+++ btree.h
@@ -0,0 +1,14 @@
+class BTree
+{
+	Node* root;
+	int degree;
+public:
+	BTree(int _degree);
+	void Traverse();
+	Node* Search(int k);
+	void ReplaceOrInsert(int k);
+private:
+	int maxKeys();
+	int minKeys();
+};
+
---
+++ node.h
@@ -0,0 +1,19 @@
+class Node
+{
+	Node** children;
+	int*	keys;
+	int len;	 // current length of the key
+	bool leaf; // Is true when node is leaf. Otherwise false
+
+	Node(int maxKeys, bool _leaf);
+	void insert(int k, int maxKeys);
+	Node* search(int k); 
+	void traverse(int level);
+	bool splitChild(int i, int maxKeys);
+	void insertKeyAt(int index, int key); 
+	void insertChildAt(int index, Node* child);
+	int find(int key); 
+	
+
+friend class BTree;
+};
